9f7851691856537d5f0764dd10e041c293793f91
yordisev <yordys12@gmail.com>
2023-06-05 15:00:46 -0500

    terminar

--- screen/Ayuda.js
+++ screen/Ayuda.js
@@ -1,32 +1,46 @@
 import React,{useState} from 'react'
-import { StatusBar } from 'expo-status-bar';
 import { View, Text,StyleSheet,TouchableOpacity,Modal,Button } from 'react-native'
-import { FontAwesome5 } from '@expo/vector-icons';
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 const Ayuda = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  showAlert = () => {
+    setModalVisible(true)
+  };
+  hideAlert = () => {
+    setModalVisible(false)
+  };
+  aceptar = () => {
+    console.log("aceptarrr")
+    setModalVisible(false)
+  };
+  cancelar = () => {
+    console.log("cancelar")
+  };
+
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-      <View style={styles.containerotro}>
-          <View style={styles.iconContainer}>
-              <FontAwesome5 name="motorcycle" size={20}/>
-          </View>
-          <Text style={styles.text}>Log Out</Text>
-      </View>
-    </TouchableOpacity>
-
-    <Modal
-        animationType='slide'
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}>
-          <View style={styles.container}>
-            <Text>Hi this is my modal.</Text>
-            <Button title="Ocultar Modal" onPress={() => setModalVisible(false)}/>
-          </View>
-      </Modal>
-      <Button title="Ver Modal" onPress={() => setModalVisible(true)} />
-      <StatusBar style="auto" />
+      {/* <Button title="Ver Modal" onPress={() => setModalVisible(true)} /> */}
+      <Button title="Ver Modal" onPress={() => showAlert()} />
+      <AwesomeAlert
+          show={modalVisible}
+          showProgress={true}
+          progressSize="large"
+          progressColor="blue"
+          title="AwesomeAlert"
+          message="I have a message for you!"
+          closeOnTouchOutside={false}
+          closeOnHardwareBackPress={false}
+          showCancelButton={true}
+          showConfirmButton={true}
+          cancelText="No, cancel"
+          cancelButtonColor="#F42A2A"
+          confirmText="Yes, delete it"
+          confirmButtonColor="#2A4CF4"
+          onCancelPressed={() => hideAlert()}
+          onConfirmPressed={() => aceptar()}
+        />
     </View>
   )
 }
@@ -36,37 +50,6 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
     justifyContent: 'center',
-    },
-    text:{
-      marginLeft: 70,
-      fontSize: 17,
-      fontWeight: '700',
-      color: 'white',
-  },
-  iconContainer:{
-      backgroundColor: 'blue',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 30,
-      width: 50,
-      height: 50,
-    },
-    containerotro:{
-      flexDirection: 'row',
-      backgroundColor: 'green',
-      fontWeight: '700',
-      marginBottom: 23,
-      marginHorizontal: 28,
-      alignItems: 'center',
-      elevation: 20,
-      borderRadius: 30,
-    },
-    bottomNavigationView: {
-      backgroundColor: '#fff',
-      width: '100%',
-      height: 250,
-      justifyContent: 'center',
-      alignItems: 'center',
     },
   });
 export default Ayuda
